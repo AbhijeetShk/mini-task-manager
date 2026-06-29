f8949a645e2c83874ee3e9ffed942f1368a62ac3
AbhijeetShk <programmerdostofficial@gmail.com>
2026-06-29 20:59:16 +0530

    feat: implement get tasks endpoint

--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createTask } from "../controllers/task.controller.js";
+import { getTasks } from "../controllers/get-tasks.js";
 
 const router = Router();
 
 router.post("/", createTask);
-
+router.get("/", getTasks);
 export default router;
